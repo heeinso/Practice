f30c0cb7ab5cbc6ebd45b888d939b1577c9b4c93
heeinso <heeinso@snu.ac.kr>
2017-12-06 23:02:34 +0900

    Complete a slots and dynamic components assignment

--- Vue.js/module09/advanced-component/src/App.vue
+++ Vue.js/module09/advanced-component/src/App.vue
@@ -2,26 +2,44 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-quote>
-          <h2 slot="title">{{ quoteTitle }}</h2>
-          <p slot="content">A wonderful quote</p>
-        </app-quote>
+        <br>
+        <button class="btn btn-primary" @click="selectedComponent='appBlue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="selectedComponent='appGreen'">Load Green Template</button>
+        <button class="btn btn-danger" @click="selectedComponent='appRed'">Load Red Template</button>
+        <hr>
+
+        <component :is="selectedComponent">
+          <p>This is the content</p>
+        </component>
+        <!--<app-blue>-->
+          <!--<p>This is the content</p>-->
+        <!--</app-blue>-->
+        <!--<app-green>-->
+          <!--<p>This is the content</p>-->
+        <!--</app-green>-->
+        <!--<app-red>-->
+          <!--<p>This is the content</p>-->
+        <!--</app-red>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Quote from './components/Quote.vue';
+  import Blue from './components/Blue.vue';
+  import Green from './components/Green.vue';
+  import Red from './components/Red.vue';
 
   export default {
     data: function() {
       return {
-        quoteTitle: 'The Quote'
+        selectedComponent: 'appBlue'
       }
     },
     components: {
-      appQuote: Quote
+      appBlue: Blue,
+      appGreen: Green,
+      appRed: Red
     }
   }
 </script>
