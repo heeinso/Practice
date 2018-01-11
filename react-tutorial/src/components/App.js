c5cfe0777dd1f53c4ddbc9cded4e6ca579a42f80
heeinso <heeinso@snu.ac.kr>
2018-01-11 16:23:05 +0900

    Implement to communicate between parent and child component

--- react-tutorial/src/components/App.js
+++ react-tutorial/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
+import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: Math.round(Math.random() * 100)
+  };
+
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue(randomValue) {
+    this.setState({
+      value: randomValue
+    });
+  }
 
   render() {
     return (
@@ -10,6 +28,8 @@ class App extends React.Component {
         <Header title={this.props.headerTitle}/>
         <Content title={this.props.contentTitle}
                  body={this.props.contentBody}/>
+        <RandomNumber number={this.state.value}
+                      onUpdate={this.updateValue}/>
       </div>
     );
   }
@@ -18,7 +38,7 @@ class App extends React.Component {
 App.defaultProps = {
   headerTitle: 'Default Header',
   contentTitle: 'Default contentTitle',
-  contentBody: 'Deafult contentBody '
+  contentBody: 'Deafult contentBody'
 }
 
 export default App;
