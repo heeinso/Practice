8b8022f15c557c537b4e28e544cb50846e07762c
heeinso <heeinso@snu.ac.kr>
2018-01-11 22:52:44 +0900

    Practice to iterate components

--- react-tutorial/src/components/App.js
+++ react-tutorial/src/components/App.js
@@ -1,44 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import Content from './Content';
-import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
 
+  render() {
+    return (
+      <Contacts/>
+    );
+  }
+}
+
+class Contacts extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      value: Math.round(Math.random() * 100)
-  };
-
-    this.updateValue = this.updateValue.bind(this);
-  }
-
-  updateValue(randomValue) {
-    this.setState({
-      value: randomValue
-    });
+      contactData: [
+        {name: "Abet", phone: "010-0000-0001"},
+        {name: "Betty", phone: "010-0000-0002"},
+        {name: "Charlie", phone: "010-0000-0003"},
+        {name: "David", phone: "010-0000-0004"}
+      ]
+    };
   }
 
   render() {
     return (
       <div>
-        <Header title={this.props.headerTitle}/>
-        <Content title={this.props.contentTitle}
-                 body={this.props.contentBody}/>
-        <RandomNumber number={this.state.value}
-                      onUpdate={this.updateValue}/>
+        <h1>Contact</h1>
+        <ul>
+          {this.state.contactData.map((contact, i) => {
+            return (<ContactInfo name={contact.name}
+                                 phone={contact.phone}
+                                 key={i}
+            />);
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-App.defaultProps = {
-  headerTitle: 'Default Header',
-  contentTitle: 'Default contentTitle',
-  contentBody: 'Deafult contentBody'
+class ContactInfo extends React.Component {
+  render() {
+    return (
+      <li>{this.props.name} {this.props.phone}</li>
+    );
+  }
 }
 
 export default App;
