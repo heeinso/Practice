181ca45c3204682ce510392016d3da8d6c53cb34
heeinso <heeinso@snu.ac.kr>
2018-01-11 15:33:00 +0900

    Practice a JSX syntax

--- react-tutorial/src/components/App.js
+++ react-tutorial/src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  sayHey() {
+    alert("hey");
+  }
+
   render() {
+    let text = "dev-server";
+
+    let pStyle = {
+      color: 'aqua',
+      backgroundColor: 'black'
+    };
+
     return (
-      <h1>Hello React!</h1>
+      <div>
+        <h1>Hello React!</h1>
+        <h2>Welcome to {text}</h2>
+        <button onClick={this.sayHey}>Click me</button>
+        <p style={pStyle}>{1 == 1 ? 'True' : 'False'}</p>
+      </div>
     );
   }
 }
