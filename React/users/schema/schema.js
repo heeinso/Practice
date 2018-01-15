3b2d23809f6b0e37542c933048424848340d246b
heeinso <heeinso@snu.ac.kr>
2018-01-15 17:54:26 +0900

    Complete schema file

--- React/users/schema/schema.js
+++ React/users/schema/schema.js
@@ -1,8 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+
+
+const users = [
+  {id: '23', firstName: 'Bill', age: 20},
+  {id: '47', firstName: 'Samantha', age: 21}
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -16,10 +26,16 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    type: UserType,
-    args: {id: {type: GraphQLString}},
-    resolve(parentValue, args) {
-      
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve(parentValue, args) {
+        return _.find(users, {id: args.id});
+      }
     }
   }
 });
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
