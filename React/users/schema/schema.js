d327ef2328374c5268d3979661b23c1aecf61fa3
heeinso <heeinso@snu.ac.kr>
2018-01-15 17:34:47 +0900

    Add a root query to schema.js

---
+++ React/users/schema/schema.js
@@ -0,0 +1,25 @@
+const graphql = require('graphql');
+
+const {
+  GraphQLObjectType
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {type: GraphQLString},
+    firstName: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    type: UserType,
+    args: {id: {type: GraphQLString}},
+    resolve(parentValue, args) {
+      
+    }
+  }
+});
