9b4d2c02091455521bbd8dbdd249f9b2184e9123
heeinso <heeinso@snu.ac.kr>
2018-01-17 22:28:25 +0900

    Add renderSongs based on key

--- React/Lyrical-GraphQL/client/components/SongList.js
+++ React/Lyrical-GraphQL/client/components/SongList.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    if (this.props.data.loading) return <div>Loading...</div>;
+
     return (
-    <div>
+    <ul className="collection">
       SongList
-    </div>
+    </ul>
     );
   }
 }
@@ -14,9 +27,10 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
