aa8d7d7a1b50ef9f9a7a184cec11d53c6b2ecbc0
heeinso <heeinso@snu.ac.kr>
2018-02-28 21:56:45 +0900

    Import AUTH_TOKEN from local storage

--- React/hackernews-react-apollo/src/index.js
+++ React/hackernews-react-apollo/src/index.js
@@ -4,12 +4,14 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 import {ApolloProvider} from 'react-apollo'
 import {ApolloClient} from 'apollo-client'
 import {HttpLink} from 'apollo-link-http'
 import {InMemoryCache} from 'apollo-cache-inmemory'
 import {BrowserRouter} from 'react-router-dom'
 import {AUTH_TOKEN} from "./constants";
+import {ApolloLink} from 'apollo-client-preset'
 
 const httpLink = new HttpLink({uri: 'http://localhost:4000'})
 
@@ -27,7 +29,7 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
