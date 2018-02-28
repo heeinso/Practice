b31d449c63cf43271242799edebd665040146536
heeinso <heeinso@snu.ac.kr>
2018-02-28 23:05:12 +0900

    Update cache

--- React/hackernews-react-apollo/src/components/LinkList.js
+++ React/hackernews-react-apollo/src/components/LinkList.js
@@ -14,8 +14,17 @@ class LinkList extends Component {
     const linksToRender = this.props.feedQuery.feed.links
 
     return (
-      <div>{linksToRender.map((link, index) => <Link key={link.id} index={index} link={link}/>)}</div>
+      <div>{linksToRender.map((link, index) => <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link}/>)}</div>
     )
+  }
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({query: FEED_QUERY})
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({query: FEED_QUERY, data})
   }
 }
 
