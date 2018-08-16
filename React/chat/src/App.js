103c211b0b085445af8ec504dbbbeae86ccbd236
heeinso <heeinso@snu.ac.kr>
2018-08-16 22:19:57 +0900

    Implement simple chatting app based on Thinking in React article

--- React/chat/src/App.js
+++ React/chat/src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import ChatWindow from './ChatWindow';
+
+const users = [{ username: 'Amy' }, { username: 'Jon' }];
 
 class App extends Component {
+  state = {
+    messages: [], //will hold this --> {usermame: 'Amy', text: 'a'}
+  };
+  onMessage = (username, message) => {
+    const newMessage = {
+      ['username']: username,
+      ['text']: message,
+    };
+    this.setState(currState => ({
+      messages: currState.messages.concat([newMessage]),
+    }));
+  };
   render() {
+    const { messages } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          {users.map(user => (
+            <ChatWindow
+              key={user.username}
+              user={user}
+              messages={messages}
+              onMessage={this.onMessage}
+            />
+          ))}
+        </div>
       </div>
     );
   }
