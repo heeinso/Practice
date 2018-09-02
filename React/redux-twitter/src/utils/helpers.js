204a2b8679c0bd2cc59c845178603d2d3faf827e
heeinso <heeinso@snu.ac.kr>
2018-09-02 18:06:35 +0900

    Implement toggling like function

--- React/redux-twitter/src/utils/helpers.js
+++ React/redux-twitter/src/utils/helpers.js
@@ -1,7 +1,6 @@
 export function formatDate(timestamp) {
   const d = new Date(timestamp);
   const time = d.toLocaleTimeString('ko-KR');
-  console.log('fucking time: ', time);
   return `${time.slice(0, 2)} ${time.substr(2, 5)}${
     time.substr(7, 1) !== ':' ? time.substr(7, 1) : ''
   } |  ${d.toLocaleDateString('ko-KR')}`;
