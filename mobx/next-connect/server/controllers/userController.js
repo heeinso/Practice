0ddaade69034ae4131fb1ac85a6cb624765f6159
reck1ess <heeinso@snu.ac.kr>
2018-12-21 22:25:00 +0900

    [next-connect] feat: Implement getting authenticated user data

--- mobx/next-connect/server/controllers/userController.js
+++ mobx/next-connect/server/controllers/userController.js
@@ -8,7 +8,15 @@ exports.getUsers = async (req, res) => {
 	res.json(users);
 };
 
-exports.getAuthUser = () => {};
+exports.getAuthUser = (req, res) => {
+	if (!req.isAuthUser) {
+		res.status(403).json({
+			message: 'You are not authenticated. Please SignIn or SignUp.',
+		});
+		return res.redirect('/signin');
+	}
+	res.json(req.user);
+};
 
 exports.getUserById = async (req, res, next, id) => {
 	const user = await User.findOne({ _id: id });
