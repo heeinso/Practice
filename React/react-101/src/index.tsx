9801256ceeaee3ef84e7d90527758facce9a2a4b
reck1ess <heeinso@snu.ac.kr>
2019-05-21 21:28:26 +0900

    [react-101] chore: Apply redux devtools to store

--- React/react-101/src/index.tsx
+++ React/react-101/src/index.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { StoreState } from './types';
+import reducer from './reducers';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import App from './App';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const store = createStore(
+	reducer,
+	composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 const rootElement: HTMLElement | null = document.getElementById('root');
 
 sagaMiddleware.run(rootSaga);
