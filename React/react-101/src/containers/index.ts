55429ede781e1d94f4c69e1f5436db40afc527b7
reck1ess <heeinso@snu.ac.kr>
2019-06-01 17:06:53 +0900

    [react-101] feat: Implement a OrderListContainer component

--- React/react-101/src/containers/index.ts
+++ React/react-101/src/containers/index.ts
@@ -2,3 +2,4 @@ export * from './Layout';
 export * from './OrderStatus';
 export * from './MonitorController';
 export * from './Notification';
+export * from './OrderListContainer';
--- React/react-101/src/pages/Order.tsx
+++ React/react-101/src/pages/Order.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { PageHeader } from '../components';
+import { OrderListContainer } from '../containers';
 
-export default class Order extends React.PureComponent {
+interface IProps {}
+
+export default class Order extends React.PureComponent<IProps> {
 	render() {
 		return (
 			<React.Fragment>
 				<PageHeader label="주문" />
+				<OrderListContainer />
 			</React.Fragment>
 		);
 	}
