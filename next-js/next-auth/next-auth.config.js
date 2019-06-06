0e10d9f962cf8191f0c2ebcb3596e60afc89061d
reck1ess <heeinso@snu.ac.kr>
2019-06-06 16:04:34 +0900

    [next-auth] fix: Fix some errors

--- next-js/next-auth/next-auth.config.js
+++ next-js/next-auth/next-auth.config.js
@@ -18,19 +18,19 @@ if (process.env.MONGO_URI) {
 }
 
 module.exports = () => {
-  return nextAuthFunctions().then(functions => {
-    return new Promise((resolve, reject) => {
-      resolve({
-        port: process.env.PORT || 3000,
-        sessionSecret: 'change-me',
-        sessionMaxAge: 60000 * 60 * 24 * 7,
-        sessionRevalidateAge: 60000,
-        serverUrl: process.env.SERVER_URL || null,
-        expressSession: expressSession,
-        sessionStore: sessionStore,
-        providers: nextAuthProviders(),
-        functions: functions
-      })
-    }
-  })
-}
+	return nextAuthFunctions().then(functions => {
+		return new Promise((resolve, reject) => {
+			resolve({
+				port: process.env.PORT || 3000,
+				sessionSecret: 'change-me',
+				sessionMaxAge: 60000 * 60 * 24 * 7,
+				sessionRevalidateAge: 60000,
+				serverUrl: process.env.SERVER_URL || null,
+				expressSession: expressSession,
+				sessionStore: sessionStore,
+				providers: nextAuthProviders(),
+				functions: functions,
+			});
+		});
+	});
+};
