0b442528a595a675d4bb076442d1e71c804c0edd
reck1ess <heeinso@snu.ac.kr>
2019-06-06 15:13:21 +0900

    [next-auth] feat: Implement a _document.js

---
+++ next-js/next-auth/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+	static async getInitialProps(props) {
+		return await Document.getInitialProps(props);
+	}
+
+	render() {
+		return (
+			<html lang="kr">
+				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link
+						rel="stylesheet"
+						href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+						integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+						crossOrigin="anonymous"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
+}
