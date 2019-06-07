b2be663ffe4c84959f4aa9654d8498b95c7ed477
reck1ess <heeinso@snu.ac.kr>
2019-06-07 21:20:21 +0900

    [react-101] feat: Add some async actions

--- React/react-101/src/actions/index.ts
+++ React/react-101/src/actions/index.ts
@@ -1,4 +1,45 @@
 import { createAction } from 'typesafe-actions';
+import { IAuthentication, FinishType } from '../store';
+import { createAsyncPayload } from './toolkit';
+
+export * from './toolkit';
+
+export const cleanupAsyncTask = createAction(
+	'@command/cleanup/async-task',
+	resolve => () => resolve()
+);
+
+export const createAsyncTask = createAction(
+	'@command/async-task/create',
+	resolve => (id: string, action: string) => {
+		return resolve({ id, action });
+	}
+);
+
+export const completeAsyncTask = createAction(
+	'@command/async-task/complete',
+	resolve => (id: string, completeStatus: FinishType = 'success') =>
+		resolve({ id, completeStatus })
+);
+
+export const requestLogin = createAction(
+	'@request/login',
+	resolve => (username: string, password: string) =>
+		resolve(createAsyncPayload({ username, password }))
+);
+
+export const requestLogout = createAction('@request/logout', resolve => () =>
+	resolve()
+);
+
+export const successLogin = createAction(
+	'@success/login',
+	resolve => (data: IAuthentication) => resolve(data)
+);
+
+export const successLogout = createAction('@success/logout', resolve => () =>
+	resolve()
+);
 
 export const addNotification = createAction('@notification/add', resolve => {
 	return (type: string, msg: string) => resolve({ type, msg });
@@ -8,6 +49,20 @@ export const showedNotification = createAction(
 	'@notification/showed',
 	resolve => {
 		return (id: number) => resolve({ id });
+	}
+);
+
+export const openNotificationCenter = createAction(
+	'@command/notification-center/open',
+	resolve => {
+		return () => resolve();
+	}
+);
+
+export const closeNotificationCenter = createAction(
+	'@command/notification-center/close',
+	resolve => {
+		return () => resolve();
 	}
 );
 
@@ -27,12 +82,16 @@ export const hideOrderTimelineChart = createAction(
 
 export const startMonitoring = createAction(
 	'@command/monitoring/start',
-	resolve => () => resolve()
+	resolve => {
+		return () => resolve();
+	}
 );
 
 export const stopMonitoring = createAction(
 	'@command/monitoring/stop',
-	resolve => () => resolve()
+	resolve => {
+		return () => resolve();
+	}
 );
 
 export const updateOrderStatus = createAction(
@@ -55,3 +114,11 @@ export const updateOrderTimeline = createAction(
 		return (success: [], failure: []) => resolve({ success, failure });
 	}
 );
+
+export const requestShopList = createAction('@request/shop/list', resolve => {
+	return () => resolve(createAsyncPayload());
+});
+
+export const responseShopList = createAction('@response/shop/list', resolve => {
+	return resp => resolve(resp);
+});
