a2b69658e87fe650b17da446da1612109cb6cc2f
reck1ess <heeinso@snu.ac.kr>
2019-06-10 23:56:46 +0900

    [react-101] feat: Add some async task workflow to saga

--- React/react-101/src/sagas/index.ts
+++ React/react-101/src/sagas/index.ts
@@ -7,12 +7,13 @@ import {
 	put,
 	call,
 	takeLatest,
+	race,
 } from 'redux-saga/effects';
 import moment from 'moment';
 import { getType } from 'typesafe-actions';
 import * as Actions from '../actions';
 import * as Api from '../apis/orders';
-import { StoreState } from '../types';
+import { IStoreState, IAuthentication } from '../store';
 
 function* fetchOrderTimeline() {
 	const {
@@ -20,6 +21,48 @@ function* fetchOrderTimeline() {
 	} = yield call(Api.fetchOrderTimeline, moment().format('YYYYMMDD'));
 
 	yield put(Actions.updateOrderTimeline(successTimeline, failureTimeline));
+}
+
+function* authenticationWorkflow() {
+	while (true) {
+		let auth = yield select(
+			(state: IStoreState): IAuthentication => state.authentication
+		);
+		let waitLogin = !auth;
+
+		while (waitLogin) {
+			try {
+				const {
+					payload: { username, password },
+				} = yield take(getType(Actions.requestLogin));
+
+				const { result } = yield call(Api.requestLogin, username, password);
+
+				waitLogin = false;
+
+				sessionStorage.setItem(
+					'authentication',
+					JSON.stringify({
+						...result,
+					})
+				);
+
+				yield put(Actions.successLogin({ ...result }));
+			} catch (e) {
+				if (e instanceof Api.ApiError) {
+					yield put(Actions.addNotification('error', e.errorMessage));
+				} else {
+					console.error(e);
+				}
+			}
+		}
+
+		yield take(getType(Actions.requestLogout));
+
+		sessionStorage.removeItem('authentication');
+
+		yield put(Actions.successLogout());
+	}
 }
 
 function* monitoringWorkflow() {
@@ -33,7 +76,7 @@ function* monitoringWorkflow() {
 					call(Api.fetchNumberOfFailedOrder),
 				]);
 
-				const { showTimeline }: StoreState = yield select();
+				const { showTimeline }: IStoreState = yield select();
 
 				if (showTimeline) {
 					yield fork(fetchOrderTimeline);
@@ -45,15 +88,15 @@ function* monitoringWorkflow() {
 						failResp.result.failure
 					)
 				);
-			} catch (err) {
-				if (err instanceof Api.ApiError) {
-					yield put(Actions.addNotification('error', err.errorMessage));
+			} catch (e) {
+				if (e instanceof Api.ApiError) {
+					yield put(Actions.addNotification('error', e.errorMessage));
 				} else {
-					console.error(err);
+					console.error(e);
 				}
 			}
 
-			const { monitoring, duration }: StoreState = yield select();
+			const { monitoring, duration }: IStoreState = yield select();
 
 			if (!monitoring) {
 				polling = false;
@@ -68,7 +111,54 @@ function* watchFetchOrderTimeline() {
 	yield takeLatest(getType(Actions.showOrderTimelineChart), fetchOrderTimeline);
 }
 
+function* watchRequestShopList() {
+	while (true) {
+		const action = yield take(getType(Actions.requestShopList));
+
+		try {
+			const resp = yield call(Api.fetchShops);
+
+			yield delay(1000);
+
+			yield put(Actions.responseShopList({ rows: resp.rows }));
+			yield put(Actions.completeAsyncTask(Actions.getAsyncId(action)));
+		} catch (e) {
+			if (e instanceof Api.ApiError) {
+				yield put(Actions.addNotification('error', e.errorMessage));
+				yield put(
+					Actions.completeAsyncTask(Actions.getAsyncId(action), 'error')
+				);
+			} else {
+				console.error(e);
+			}
+		}
+	}
+}
+
+function* watchAsyncTask() {
+	while (true) {
+		const action = yield take('*');
+
+		if (Actions.isAsyncAction(action)) {
+			yield put(
+				Actions.createAsyncTask(Actions.getAsyncId(action), action.type)
+			);
+		}
+	}
+}
+
+function* cleanupAsyncTaskStats() {
+	while (true) {
+		yield delay(1000 * 60);
+		yield put(Actions.cleanupAsyncTask());
+	}
+}
+
 export default function*() {
+	yield fork(cleanupAsyncTaskStats);
+	yield fork(watchAsyncTask);
 	yield fork(monitoringWorkflow);
+	yield fork(authenticationWorkflow);
 	yield fork(watchFetchOrderTimeline);
+	yield fork(watchRequestShopList);
 }
