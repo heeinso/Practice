f11724b729400eade1344cd18dd00e09c9e9f419
reck1ess <heeinso@snu.ac.kr>
2019-06-10 23:53:14 +0900

    [react-101] feat: Apply PrivateRoute to Router component

--- React/react-101/src/router/index.tsx
+++ React/react-101/src/router/index.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
-import { ConnectedRouter } from 'connected-react-router';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { DefaultLayout } from '../containers';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+	DefaultLayout,
+	FullSizeLayout,
+	NotificationContainer,
+} from '../containers';
 import * as Pages from '../pages';
+import PrivateRoute from './PrivateRoute';
 
-interface IProps {}
+interface IProps {
+	children?: React.ReactNode;
+}
 
 const Router: React.FC<IProps> = () => {
 	return (
 		<BrowserRouter>
-			<DefaultLayout>
-				<Route exact path="/" component={Pages.Dashboard} />
-				<Route exact path="/orders" component={Pages.Order} />
-			</DefaultLayout>
+			<NotificationContainer />
+			<Switch>
+				<Route exact path="/login">
+					<FullSizeLayout>
+						<Pages.Login />
+					</FullSizeLayout>
+				</Route>
+				<DefaultLayout>
+					<Switch>
+						<PrivateRoute exact path="/" page={Pages.Dashboard} />
+						<PrivateRoute exact path="/orders" page={Pages.Order} />
+						<PrivateRoute exact path="/shops" page={Pages.Shops} />
+						<Route component={Pages.PageNotFound} />
+					</Switch>
+				</DefaultLayout>
+			</Switch>
 		</BrowserRouter>
 	);
 };
