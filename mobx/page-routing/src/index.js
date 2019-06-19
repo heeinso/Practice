c0645b0bb0375a2e08b4c08cca527826f8de127b
reck1ess <heeinso@snu.ac.kr>
2019-06-19 23:37:06 +0900

    [page-routing] feat: Apply CheckoutWorkFlow into App

--- mobx/page-routing/src/index.js
+++ mobx/page-routing/src/index.js
@@ -1,4 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Router, Switch } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import { CheckoutWorkflow } from './checkout';
+import { Paper } from '@material-ui/core';
+import { ShowCart } from './showCart';
+import {
+	ConfirmDescription,
+	PaymentDescription,
+	ShoppingDescription,
+	TemplateStepComponent,
+	TrackOrderDescription,
+} from './shared';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const workflow = new CheckoutWorkflow();
+
+class App extends React.Component {
+	render() {
+		return (
+			<Paper elevation={2} style={{ padding: 20 }}>
+				<Router history={workflow.tracker.history}>
+					<Switch>
+						<Route
+							exact
+							path={'/'}
+							component={() => (
+								<TemplateStepComponent
+									title={'MobX Shop'}
+									renderDetails={ShoppingDescription}
+									operationTitle={'View Cart'}
+								/>
+							)}
+						/>
+						<Route exact path={'/cart'} component={ShowCart} />
+						<Route
+							exact
+							path={'/payment'}
+							component={() => (
+								<TemplateStepComponent
+									title={'Choose Payment'}
+									renderDetails={PaymentDescription}
+									operationTitle={'Confirm'}
+								/>
+							)}
+						/>
+						<Route
+							exact
+							path={'/confirm'}
+							component={() => (
+								<TemplateStepComponent
+									title={'Your order is confirmed'}
+									operationTitle={'Track Order'}
+									renderDetails={ConfirmDescription}
+								/>
+							)}
+						/>
+						<Route
+							exact
+							path={'/track'}
+							component={() => (
+								<TemplateStepComponent
+									title={'Track your order'}
+									operationTitle={'Continue Shopping'}
+									renderDetails={TrackOrderDescription}
+								/>
+							)}
+						/>
+					</Switch>
+				</Router>
+			</Paper>
+		);
+	}
+}
+ReactDOM.render(
+	<Provider store={workflow}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
