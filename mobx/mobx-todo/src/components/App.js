31c74a1f03e0923536fbadec178228a028f3c791
reck1ess <heeinso@snu.ac.kr>
2019-06-20 23:49:33 +0900

    [mobx-todo] feat: Apply todoStore to App

--- mobx/mobx-todo/src/components/App.js
+++ mobx/mobx-todo/src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import DevTools from 'mobx-react-devtools';
-import TodoList from './TodoList';
-import todoStore from '../stores/TodoStore';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<TodoList store={todoStore} />
 				<DevTools />
 			</div>
 		);
--- mobx/mobx-todo/src/index.js
+++ mobx/mobx-todo/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './components/App';
+import TodoStore from './stores/TodoStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={TodoStore}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
