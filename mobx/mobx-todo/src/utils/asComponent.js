eb610954e3a59c72886668bc4e7d0ed511ba0ba9
reck1ess <heeinso@snu.ac.kr>
2019-06-21 23:50:10 +0900

    [mobx-todo] feat: Implement a helper to enable function be a react component

---
+++ mobx/mobx-todo/src/utils/asComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function asComponent(fn) {
+    return class ExampleComponent extends React.Component {
+        render() {
+            return fn() || null;
+        }
+    };
