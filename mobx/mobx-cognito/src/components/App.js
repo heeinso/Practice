a2d2b2e464540f5f6f7d984a21e4be9978e4a57f
reck1ess <heeinso@snu.ac.kr>
2019-07-01 23:48:08 +0900

    [mobx-cognito] refactor: Apply react-router to App.js

---
+++ mobx/mobx-cognito/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+
+import Header from './lib/Header';
+import Home from './Home';
+
+@inject('authStore', 'commonStore')
+@withRouter
+@observer
+export default class App extends React.Component {
+	componentWillMount() {
+		this.props.authStore.verifySession();
+	}
+
+	render() {
+		if (this.props.commonStore.appLoaded) {
+			return (
+				<div>
+					<Header />
+					<Switch>
+						<Route path="/" component={Home} />
+					</Switch>
+				</div>
+			);
+		}
+		return null;
+	}
+}
