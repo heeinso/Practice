3bd80f2c47e5b5aa8b883cfbdbf63e53b45d8596
reck1ess <heeinso@snu.ac.kr>
2019-07-06 22:03:57 +0900

    [mobx-cognito] feat: Apply react-router to app

--- mobx/mobx-cognito/src/components/App.js
+++ mobx/mobx-cognito/src/components/App.js
@@ -5,25 +5,37 @@ import { inject, observer } from 'mobx-react';
 import Header from './Common/Header';
 import Home from './Home';
 
+import Login from './Account/Login';
+import Logout from './Account/Logout';
+import Confirm from './Account/Confirm';
+import Register from './Account/Register';
+import Settings from './Account/Settings';
+
 @inject('authStore', 'commonStore')
 @withRouter
 @observer
-export default class App extends React.Component {
+class App extends React.Component {
 	componentWillMount() {
 		this.props.authStore.verifySession();
 	}
 
 	render() {
-		if (this.props.commonStore.appLoaded) {
-			return (
-				<div>
-					<Header />
-					<Switch>
-						<Route path="/" component={Home} />
-					</Switch>
-				</div>
-			);
-		}
-		return null;
+		const { appLoaded } = this.props.commonStore;
+
+		return appLoaded ? (
+			<div>
+				<Header />
+				<Switch>
+					<Route path="/settings" component={Settings} />
+					<Route path="/logout" component={Logout} />
+					<Route path="/login" component={Login} />
+					<Route path="/register/confirm" component={Confirm} />
+					<Route path="/register" component={Register} />
+					<Route path="/" component={Home} />
+				</Switch>
+			</div>
+		) : null;
 	}
 }
+
+export default App;
