d2f29244af4d7c56727dfe56f49497440cacf142
reck1ess <heeinso@snu.ac.kr>
2019-07-06 22:03:36 +0900

    [mobx-cognito] feat: Inject mobx store to app

--- mobx/mobx-cognito/src/index.js
+++ mobx/mobx-cognito/src/index.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import { Provider } from 'mobx-react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import authStore from './stores/authStore';
+import commonStore from './stores/commonStore';
+
+const stores = {
+	authStore,
+	commonStore,
+};
+
+// For easier debugging
+window._____APP_STATE_____ = stores;
+
+ReactDOM.render(
+	<Provider {...stores}>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</Provider>,
+	document.getElementById('root')
+);
