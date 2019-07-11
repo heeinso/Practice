cad383746c2e001769392206cde7f1dbf2587f84
reck1ess <heeinso@snu.ac.kr>
2019-07-11 23:55:43 +0900

    [gatsby-i18n] refactor: Modify a selectLanguage component

--- gatsby/gatsby-i18n/src/components/SelectLanguage.js
+++ gatsby/gatsby-i18n/src/components/SelectLanguage.js
@@ -3,33 +3,33 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import { FormattedMessage } from 'react-intl';
 
-const SelectLanguage = (props) => {
-  const links = props.langs.map(lang =>
-    <Link to={lang.link} key={lang.langKey} style={{
-      color: 'white'
-    }}>
-      <li selected={lang.selected}>
-        {lang.langKey}
-      </li>
-    </Link>
-  );
-
-  return (
-    <section>
-      <header style={{
-        color: 'white'
-      }}>
-        <FormattedMessage id="selectLanguage" />
-      </header>
-      <ul>
-        {links}
-      </ul>
-    </section>
-  );
+const SelectLanguage = ({ langs }) => {
+	return (
+		<section>
+			<header
+				style={{
+					color: 'white',
+				}}>
+				<FormattedMessage id="selectLanguage" />
+			</header>
+			<ul>
+				{
+					<Link
+						to={lang.link}
+						key={lang.langKey}
+						style={{
+							color: 'white',
+						}}>
+						<li selected={lang.selected}>{lang.langKey}</li>
+					</Link>
+				}
+			</ul>
+		</section>
+	);
 };
 
 SelectLanguage.propTypes = {
-  langs: PropTypes.array
+	langs: PropTypes.array,
 };
 
 export default SelectLanguage;
