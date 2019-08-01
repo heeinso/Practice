92fa91a15f9aebebbbe0f63cae470c87b4e65f0e
reck1ess <heeinso@snu.ac.kr>
2019-08-01 23:02:36 +0900

    [react-hooks] feat: implement a Todo List

--- React/react-hooks/src/App.js
+++ React/react-hooks/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Form from './Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
+	const [todos, setTodos] = React.useState([]);
 
-export default App;
+	const toggleComplete = i =>
+		setTodos(
+			todos.map((todo, k) =>
+				k === i ? { ...todo, complete: !todo.complete } : todo
+			)
+		);
+
+	return (
+		<div className="App">
+			<Form
+				onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+			/>
+			<div>
+				{todos.map(({ text, complete }, i) => (
+					<div
+						key={text}
+						onClick={() => toggleComplete(i)}
+						style={{
+							textDecoration: complete ? 'line-through' : '',
+						}}>
+						{text}
+					</div>
+				))}
+			</div>
+			<button onClick={() => setTodos([])}>reset</button>
+		</div>
+	);
+};
