1e6807417c4d914b077c18ec05c1fc051111cc25
reck1ess <heeinso@snu.ac.kr>
2019-12-17 23:53:39 +0900

    refactor: Organize index.js based on Provider

--- redux/conduit/src/index.js
+++ redux/conduit/src/index.js
@@ -1,49 +1,24 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import App from './components/App';
+
 const defaultState = {
-	checked: false,
+	appName: `conduit`,
+	articles: null,
 };
 
 const reducer = (state = defaultState, action) => {
-	switch (action.type) {
-		case 'TOGGLE':
-			return { ...state, checked: !state.checked };
-	}
 	return state;
 };
 
 const store = createStore(reducer);
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
-
-	componentWillMount() {
-		store.subscribe(() => this.setState(store.getState()));
-	}
-
-	handleClick = () => store.dispatch({ type: 'TOGGLE' });
-
-	render() {
-		return (
-			<div>
-				<h1>TODO</h1>
-				<div>
-					Learn Redux&nbsp;
-					<input
-						type="checkbox"
-						checked={!!this.state.checked}
-						onClick={this.handleClick}
-					/>
-				</div>
-				{this.state.checked && <h2>Done!</h2>}
-			</div>
-		);
-	}
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
