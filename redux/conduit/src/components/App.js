f18acc01b8cb40d6e0de866d3bea74d7380a6677
reck1ess <heeinso@snu.ac.kr>
2019-12-19 23:51:42 +0900

    feat: Implement a promiseMiddleware

--- redux/conduit/src/components/App.js
+++ redux/conduit/src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Header from './Header';
+
 const mapStateToProps = state => ({
 	appName: state.appName,
 });
 
 class App extends React.Component {
 	render() {
-		return <div>{this.props.appName}</div>;
+		return (
+			<div>
+				<Header appName={this.props.appName} />
+			</div>
+		);
 	}
 }
 
