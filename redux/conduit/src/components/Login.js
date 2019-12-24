5a32c540be4c829e18f9baaeaf44868723ac2099
reck1ess <heeinso@snu.ac.kr>
2019-12-24 12:57:18 +0900

    refactor: Modify a Login component

--- redux/conduit/src/components/Login.js
+++ redux/conduit/src/components/Login.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import agent from '../agent';
+import ListErrors from './ListErrors';
+
+const mapStateToProps = state => ({ ...state.auth });
+
+const mapDispatchToProps = dispatch => ({
+	onChangeEmail: value =>
+		dispatch({ type: 'UPDATE_FIELD_AUTH', key: 'email', value }),
+	onChangePassword: value =>
+		dispatch({ type: 'UPDATE_FIELD_AUTH', key: 'password', value }),
+	onSubmit: (email, password) =>
+		dispatch({ type: 'LOGIN', payload: agent.Auth.login(email, password) }),
+});
 
 class Login extends React.Component {
+	constructor() {
+		super();
+		this.changeEmail = e => this.props.onChangeEmail(e.target.value);
+		this.changePassword = e => this.props.onChangePassword(e.target.value);
+		this.submitForm = (email, password) => e => {
+			e.preventDefault();
+			this.props.onSubmit(email, password);
+		};
+	}
+
 	render() {
+		const { email, password } = this.props;
 		return (
 			<div className="auth-page">
 				<div className="container page">
@@ -10,16 +36,19 @@ class Login extends React.Component {
 						<div className="col-md-6 offset-md-3 col-xs-12">
 							<h1 className="text-xs-center">Sign In</h1>
 							<p className="text-xs-center">
-								<a>Need an account?</a>
+								<Link to="register">Need an account?</Link>
 							</p>
 
-							<form>
+							<ListErrors errors={this.props.errors} />
+							<form onSubmit={this.submitForm(email, password)}>
 								<fieldset>
 									<fieldset className="form-group">
 										<input
 											className="form-control form-control-lg"
 											type="email"
 											placeholder="Email"
+											value={email}
+											onChange={this.changeEmail}
 										/>
 									</fieldset>
 
@@ -28,12 +57,15 @@ class Login extends React.Component {
 											className="form-control form-control-lg"
 											type="password"
 											placeholder="Password"
+											value={password}
+											onChange={this.changePassword}
 										/>
 									</fieldset>
 
 									<button
 										className="btn btn-lg btn-primary pull-xs-right"
-										type="submit">
+										type="submit"
+										disabled={this.props.inProgress}>
 										Sign in
 									</button>
 								</fieldset>
@@ -46,7 +78,4 @@ class Login extends React.Component {
 	}
 }
 
-export default connect(
-	() => ({}),
-	() => ({})
-)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
