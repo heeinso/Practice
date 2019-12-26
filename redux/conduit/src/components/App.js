922f4edfa0985e3513816cc1288545edae85dfad
reck1ess <heeinso@snu.ac.kr>
2019-12-26 21:57:38 +0900

    refactor: Modify an App.js

--- redux/conduit/src/components/App.js
+++ redux/conduit/src/components/App.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import agent from '../agent';
 import Header from './Header';
 
 const mapStateToProps = state => ({
-	appName: state.appName,
+	appName: state.common.appName,
+	currentUser: state.common.currentUser,
+	redirectTo: state.common.redirectTo,
+});
+
+const mapDispatchToProps = dispatch => ({
+	onLoad: (payload, token) => dispatch({ type: 'APP_LOAD', payload, token }),
+	onRedirect: () => dispatch({ type: 'REDIRECT' }),
 });
 
 class App extends React.Component {
+	componentWillMount() {
+		const token = window.localStorage.getItem('jwt');
+		if (token) {
+			agent.setToken(token);
+		}
+
+		this.props.onLoad(token ? agent.Auth.current() : null, token);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.redirectTo) {
+			this.context.router.replace(nextProps.redirectTo);
+			this.props.onRedirect();
+		}
+	}
 	render() {
 		return (
 			<div>
-				<Header appName={this.props.appName} />
+				<Header
+					appName={this.props.appName}
+					currentUser={this.props.currentUser}
+				/>
+				{this.props.children}
 			</div>
 		);
 	}
 }
 
-export default connect(mapStateToProps, () => ({}))(App);
+App.contextType = {
+	router: React.PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
