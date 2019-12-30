553e2e61b1a1d4f519ff98178a02b577a5022375
reck1ess <heeinso@snu.ac.kr>
2019-12-30 18:18:13 +0900

    feat: Add some internal API related to Tag

--- redux/conduit/src/agent.js
+++ redux/conduit/src/agent.js
@@ -1,5 +1,5 @@
-import _superagent from 'superagent';
 import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
@@ -8,12 +8,18 @@ const API_ROOT = 'https://conduit.productionready.io/api';
 const responseBody = res => res.body;
 
 let token = null;
-
 const tokenPlugin = req => {
-	token && req.set('authorization', `Token ${token}`);
+	if (token) {
+		req.set('authorization', `Token ${token}`);
+	}
 };
 
 const requests = {
+	del: url =>
+		superagent
+			.del(`${API_ROOT}${url}`)
+			.use(tokenPlugin)
+			.then(responseBody),
 	get: url =>
 		superagent
 			.get(`${API_ROOT}${url}`)
@@ -29,27 +35,28 @@ const requests = {
 			.put(`${API_ROOT}${url}`, body)
 			.use(tokenPlugin)
 			.then(responseBody),
-	del: url =>
-		superagent
-			.del(`${API_ROOT}${url}`)
-			.use(tokenPlugin)
-			.then(responseBody),
 };
 
+const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
+const encode = encodeURIComponent;
+
 const Articles = {
-	all: page => requests.get(`/articles?limit=10`),
+	all: page => requests.get(`/articles?${limit(10, page)}`),
 	byAuthor: (author, page) =>
-		requests.get(`/articles?author=${encodeURIComponent(author)}&limit=5`),
+		requests.get(`/articles?author=${encode(author)}&${limit(10, page)}`),
+	byTag: (tag, page) =>
+		requests.get(`/articles?tag=${encode(tag)}&${limit(10, page)}`),
 	del: slug => requests.del(`/articles/${slug}`),
 	favoritedBy: (author, page) =>
-		requests.get(`/articles?favorited=${encodeURIComponent(author)}&limit=5`),
+		requests.get(`/articles?favorited=${encode(author)}&${limit(10, page)}`),
+	feed: page => requests.get(`/articles/feed?${limit(10, page)}`),
 	get: slug => requests.get(`/articles/${slug}`),
 };
 
 const Auth = {
-	current: () => requests.get(`/user`),
+	current: () => requests.get('/user'),
 	login: (email, password) =>
-		requests.post(`/user/login`, { user: { email, password } }),
+		requests.post('/users/login', { user: { email, password } }),
 	register: (username, email, password) =>
 		requests.post('/users', { user: { username, email, password } }),
 	save: user => requests.put('/user', { user }),
@@ -69,12 +76,17 @@ const Profile = {
 	unfollow: username => requests.del(`/profiles/${username}/follow`),
 };
 
+const Tags = {
+	getAll: () => requests.get('/tags'),
+};
+
 export default {
 	Articles,
 	Auth,
 	Comments,
 	Profile,
+	Tags,
 	setToken: _token => {
-		token: _token;
+		token = _token;
 	},
 };
