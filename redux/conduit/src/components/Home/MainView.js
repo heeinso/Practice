631f233c57a02a6e263147eadcbbe735f4fbff00
reck1ess <heeinso@snu.ac.kr>
2019-12-30 17:40:12 +0900

    chore: Add some reducers to redux store

--- redux/conduit/src/components/Home/MainView.js
+++ redux/conduit/src/components/Home/MainView.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import ArticleList from '../ArticleList';
 
 const mapStateToProps = state => ({
-	articles: state.home.articles,
+	...state.articleList,
 });
 
 const MainView = props => (
