126ea325752e496ea705018885af55e02efd15c6
reck1ess <heeinso@snu.ac.kr>
2020-01-01 20:49:59 +0900

    chore: Add some routes to index.js

--- redux/conduit/src/index.js
+++ redux/conduit/src/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import App from './components/App';
-import Article from './components/Article';
-import Home from './components/Home';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import ProfileFavorites from './components/ProfileFavorites';
-import Register from './components/Register';
-import Settings from './components/Settings';
-import store from './store';
+import App from "./components/App";
+import Article from "./components/Article";
+import Editor from "./components/Editor";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import ProfileFavorites from "./components/ProfileFavorites";
+import Register from "./components/Register";
+import Settings from "./components/Settings";
+import store from "./store";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router history={hashHistory}>
-			<Route path="/" component={App}>
-				<IndexRoute component={Home} />
-				<Route path="/login" component={Login} />
-				<Route path="register" component={Register} />
-				<Route path="settings" component={Settings} />
-				<Route path="article/:id" component={Article} />
-				<Route path="@:username" component={Profile} />
-				<Route path="@:username/favorites" component={ProfileFavorites} />
-			</Route>
-		</Router>
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="register" component={Register} />
+        <Route path="settings" component={Settings} />
+        <Route path="article/:id" component={Article} />
+        <Route path="@:username" component={Profile} />
+        <Route path="@:username/favorites" component={ProfileFavorites} />
+        <Route path="editor" component={Editor} />
+        <Route path="editor/:slug" component={Editor} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
