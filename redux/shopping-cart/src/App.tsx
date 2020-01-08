36034bd106ea5579796225d301434995b1022cc3
reck1ess <heeinso@snu.ac.kr>
2020-01-08 23:26:09 +0900

    [shopping-cart] refactor: Modify index.tsx

--- redux/shopping-cart/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App: React.FC = () => {
-  return <div className="App">Shopping cart</div>;
-};
-
-export default App;
--- redux/shopping-cart/src/index.tsx
+++ redux/shopping-cart/src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
-import App from "./App";
+import reducer from "./reducers";
+import { getAllProducts } from "./actions";
+import App from "./containers/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
+store.dispatch(getAllProducts());
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
